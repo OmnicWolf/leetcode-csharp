5d48ce8ca5c2f2f2ecf816b4ebc121ec34436f2d
Ryo Light <87935417+OmnicWolf@users.noreply.github.com>
2021-10-19 22:53:56 -0400

    Added first attempt for 92. Reverse Linked List II

--- Leetcode/Medium/ReverseLinkedListII.cs
+++ Leetcode/Medium/ReverseLinkedListII.cs
@@ -19,6 +19,14 @@ namespace Leetcode.Medium
         {
             if (left == right || head == null) return head;
 
+            /* 
+             * Why insert an anchor node before head?
+             * 
+             * It's an elegant solution for the edge case where reversal starts on the first node.
+             * With an anchor, the list is guaranteed to have a left segment before the reversal area
+             * to allow the shifting node to move up front.
+             */
+
             ListNode anchor = new ListNode(-1, head);
 
             ListNode leftSegmentTail = anchor;
@@ -39,13 +47,44 @@ namespace Leetcode.Medium
 
             return anchor.next;
         }
+
+
+        /* Runtime: 139 ms, faster than 19.69% of C# online submissions for Reverse Linked List II.
+         * Memory Usage: 36.1 MB, less than 9.85% of C# online submissions for Reverse Linked List II.
+         */
+        public ListNode ReverseBetween1stAttempt(ListNode head, int left, int right)
+        {
+            if (left == right || head == null) return head;
+
+            ListNode currNode = head;
+            ListNode leftSegmentTail = head;
+
+            int i = 1;
+
+            while (i < left)
+            {
+                leftSegmentTail = currNode;
+                currNode = currNode.next;
+                ++i;
+            }
+
+            ListNode reversedSegmentTail = currNode;
+
+            ListNode reversedSegmentHead = null, next;
+
+            while (i < right + 1)
+            {
+                next = currNode.next;
+                currNode.next = reversedSegmentHead;
+                reversedSegmentHead = currNode;
+                currNode = next;
+                ++i;
+            }
+
+            leftSegmentTail.next = reversedSegmentHead;
+            reversedSegmentTail.next = currNode;
+
+            return left > 1 ? head : reversedSegmentHead;
+        }
     }
 }
-
-/* 
- * Why insert an anchor node before head?
- * 
- * It's an elegant solution for the edge case where 'm (left)' is the first node.
- * With an anchor, the list is guaranteed to have a left segment before the reversal area
- * to allow the shifting node to move up front.
- */
